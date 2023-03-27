0f6a945b9a91bcd874e10010460ec84e4597e7c4
Gabriel <djabreu_@hotmail.com>
2023-03-26 21:56:37 -0400

    Cambios en formatos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,49 @@
 import React from "react";
 import { useState } from "react";
 
-function Form(props){
+function Form(props) {
+  //set the state of the name variable
+  const [name, setName] = useState("");
 
-//set the state of the name variable
-const [name, setName] = useState("");
-
-// function handleSubmit(event){ preventDefault();} 
-    function handleSubmit(e){
-        e.preventDefault();
-        if(name === ""){
-        alert("Please enter a task");
-        }
-        else{
-        //data from the form to app.js
-        props.addTast(name);
-        setName("");
-        }
+  // function handleSubmit(event){ preventDefault();}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name === "") {
+      alert("Please enter a task");
+    } else {
+      //data from the form to app.js
+      props.addTast(name);
+      setName("");
     }
+  }
 
-//function onchange to set the state of the name variable
-function handleChange(e){
+  //function onchange to set the state of the name variable
+  function handleChange(e) {
     setName(e.target.value);
     console.log(e.target.value);
-};
+  }
 
-    return(
-     <form onSubmit={handleSubmit}>   
-    <h2 className="label-wrapper">
-    <label htmlFor="new-todo-input" className="label__lg">
-      What needs to be done?
-    </label>
-  </h2>
-  <input
-    type="text"
-    id="new-todo-input"
-    className="input input__lg"
-    name="text"
-    autoComplete="off"
-    value={name}
-    onChange={handleChange}
-  />
-  <button type="submit" className="btn btn__primary btn__lg">
-    Add
-  </button>
-  </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className='label-wrapper'>
+        <label htmlFor='new-todo-input' className='label__lg'>
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type='text'
+        id='new-todo-input'
+        className='input input__lg'
+        name='text'
+        autoComplete='off'
+        value={name}
+        onChange={handleChange}
+      />
+      <button type='submit' className='btn btn__primary btn__lg'>
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default Form;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const DATA = [
-  {id: "todo-0", name: "Eat", completed: true},
-  {id: "todo-1", name: "Sleep", completed: false},
-  {id: "todo-2", name: "Repeat", completed: false}
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
 ];
-const log = console.log(DATA);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<React.StrictMode>
-    <App  tasks={DATA}/>
-</React.StrictMode>
-);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>
+);
